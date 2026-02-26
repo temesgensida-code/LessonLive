697e361bf7845e4d8ce31be70ccc71c154322260
temesgen-cell <temesgensids@gmail.com>
2026-02-26 04:18:49 +0300

    ...

--- frontend/src/components/LiveClassSidePanel.jsx
+++ frontend/src/components/LiveClassSidePanel.jsx
@@ -1,32 +1,21 @@
-import { LiveKitRoom, RoomAudioRenderer } from '@livekit/components-react'
+import { RoomAudioRenderer } from '@livekit/components-react'
 import UsernameChat from '../UsernameChat'
 import LiveAudioButton from './LiveAudioButton'
-import { LIVEKIT_SERVER_URL } from './apiClient'
 
-function LiveClassSidePanel({ token }) {
+function LiveClassSidePanel({ owned }) {
   return (
     <div className="live-side-shell">
-      <LiveKitRoom
-        connect
-        video={false}
-        audio
-        token={token}
-        serverUrl={LIVEKIT_SERVER_URL}
-        data-lk-theme="default"
-        className="live-room"
-      >
-        <div className="live-side-content">
-          <h3>Live Class Chat</h3>
-          <div className="live-chat-box">
-            <UsernameChat />
-          </div>
-          <div className="live-audio-box">
-            <h4>Audio Chat</h4>
-            <LiveAudioButton />
-          </div>
+      <div className="live-side-content">
+        <h3>Live Class Chat</h3>
+        <div className="live-chat-box">
+          <UsernameChat />
         </div>
-        <RoomAudioRenderer />
-      </LiveKitRoom>
+        <div className="live-audio-box">
+          <h4>Audio Chat</h4>
+          <LiveAudioButton owned={owned} />
+        </div>
+      </div>
+      <RoomAudioRenderer />
     </div>
   )
 }
