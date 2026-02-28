81d1d917904c4dfbbf7dfd865b796324a06c40f2
temesgen-cell <temesgensids@gmail.com>
2026-02-28 05:15:56 +0300

    asgi import location changes to deployment

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 from backend.routing import websocket_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 django_asgi_application = get_asgi_application()
 
