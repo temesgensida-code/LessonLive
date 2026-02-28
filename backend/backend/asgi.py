b1a4a2ab05032b96de5cad50fd2bcac34c6dc39f
temesgen-cell <temesgensids@gmail.com>
2026-02-28 05:28:55 +0300

    asgi reordered

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -14,9 +14,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-from backend.routing import websocket_application
-
 django_asgi_application = get_asgi_application()
+
+from backend.routing import websocket_application
 
 application = ProtocolTypeRouter(
 	{
